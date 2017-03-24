0ede37a00223e94ce7fac0c897d72f513ffc9d5e
Joshua McCormick <jjm2g13@ecs.soton.ac.uk>
2017-03-24 11:47:02 +0000

    Add player with cursor, Questions class

---
+++ Rumplefrogsgruff/Assets/Player/Scripts/Question.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question {
+
+	public enum Items { KNIFE, AXE, PEN, PAINTING, BOOKS };
+	private string text;
+	private Dictionary<int, string> subject_response;
+	private List<Question> blocks;
+
+	public Question(string text, List<Question> blocks = null){
+		subject_response = new Dictionary<int, string>();
+		if (blocks == null) {
+			blocks = new List<Question> ();
+		}
+		this.text = text;
+	}
+
+	public void addResponse(int item, string response){
+		subject_response.Add (item, response);
+	}
+
+	public bool removeResponse(int item){
+		if(subject_response.ContainsKey (item)){
+			subject_response.Remove (item);
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	public string getResponse(int item){
+		return subject_response [item];
+	}
+
+	public void addBlock(Question toBlock){
+		blocks.Add (toBlock);
+	}
+
+	public bool doesBlock(Question q){
+		return blocks.Contains (q);
+	}
+}
